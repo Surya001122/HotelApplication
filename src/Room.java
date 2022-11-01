f47585573c3f8138cc38bc98bae2293be0a8a389
Surya prakash R <suryareigns001@gmail.com>
2022-11-01 18:55:36 +0530

    Room Updates

--- src/Room.java
+++ src/Room.java
@@ -1,7 +1,9 @@
 import javafx.scene.input.InputMethodTextRun;
+
 import java.time.LocalDate;
 import java.util.*;
 import java.time.LocalDateTime;
+
 public abstract class Room {
     Scanner sc = new Scanner(System.in);
     private int roomNumber;
@@ -12,6 +14,7 @@ public abstract class Room {
     private int rent;
     private int roomMobileNumber;
     private static int roomServiceNumber = 123;
+
     public Room(int roomNumber, String roomType, int floorNumber, int totalBeds, int rent, int roomMobileNumber) {
         this.roomNumber = roomNumber;
         this.roomType = roomType;
@@ -87,12 +90,14 @@ public abstract class Room {
     }
 
     public void displayRoomInfo() {
-        System.out.println("\n\nRoom Type : "+this.roomType+
-                "\nRoom Number : "+this.roomNumber+
-                "\nFloorNumber : "+this.floorNumber+
-                "\nTotal beds : "+this.totalBeds+
-                "\nRoom rent : "+this.rent);
+        System.out.println("\n\nRoom Type : " + this.roomType +
+                "\nRoom Number : " + this.roomNumber +
+                "\nFloorNumber : " + this.floorNumber +
+                "\nTotal beds : " + this.totalBeds +
+                "\nRoom rent : " + this.rent);
     }
+
     abstract void viewAvailableFacilities();// abstract method, as the availability of facilities changes for different rooms and there is no need to implement this method in general class Room.
+
     abstract int calculateRoomRent(int days);
 }
